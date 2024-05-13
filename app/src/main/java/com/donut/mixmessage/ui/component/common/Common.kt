fbd7d37e992770fb859a0f005693cd8c085bb790
InvertGeek <admin@qq.com>
2024-05-13 18:09:56 +0800

    优化滚动

--- app/src/main/java/com/donut/mixmessage/ui/component/common/Common.kt
+++ app/src/main/java/com/donut/mixmessage/ui/component/common/Common.kt
@@ -11,8 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.outlined.Done
@@ -22,10 +20,6 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -109,9 +103,11 @@ fun ClearableTextField(
                 Icons.Filled.Clear,
                 contentDescription = "Clear text",
                 tint = LightColorScheme.primary,
-                modifier = Modifier.clickable {
-                    onValueChange(TextFieldValue())
-                }.padding(4.dp)
+                modifier = Modifier
+                    .clickable {
+                        onValueChange(TextFieldValue())
+                    }
+                    .padding(4.dp)
             )
         }
     }
@@ -121,7 +117,6 @@ fun ClearableTextField(
 fun SingleSelectItemList(items: List<String>, currentOption: String, onSelect: (String) -> Unit) {
     LazyColumn(
         modifier = Modifier
-            .verticalScroll(rememberScrollState())
             .heightIn(0.dp, 400.dp),
     ) {
         items(items.size) { item ->

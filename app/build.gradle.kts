98cebf94b15aa72233ae6698c72c8bda44825544
InvertGeek <admin@qq.com>
2024-07-08 21:52:45 +0800

    更新成语数据

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.donut.mixmessage"
         minSdk = 24
         targetSdk = 34
-        versionCode = 63
-        versionName = "1.7.0"
+        versionCode = 64
+        versionName = "1.7.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

61862c38d0ccb4fe3ee0663aed9bf5f4cbf6f172
InvertGeek <admin@qq.com>
2024-07-14 12:12:13 +0800

    更新版本号

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.donut.mixmessage"
         minSdk = 24
         targetSdk = 34
-        versionCode = 74
-        versionName = "1.7.11"
+        versionCode = 75
+        versionName = "1.7.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

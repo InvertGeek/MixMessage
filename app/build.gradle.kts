2c103795fd60c9cc9033d2f21ab0e797aaf2cf47
Evan You <yyx990803@gmail.com>
2025-01-06 10:19:01 +0800

    更新表情列表

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.donut.mixmessage"
         minSdk = 24
         targetSdk = 34
-        versionCode = 86
-        versionName = "1.12.1"
+        versionCode = 87
+        versionName = "1.12.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
